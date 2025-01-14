bbf84fd50d0d630c1ad288ab68aa559f062c7552
shutstart <chirag.cool200@gmail.com>
2025-01-14 11:16:30 +0100

    added the init done field, corresponding tests and fixed the check for is init done

--- test/migration/migrate_test.go
+++ test/migration/migrate_test.go
@@ -501,6 +501,13 @@ func TestApi_Migrate_test(t *testing.T) {
 		r.EqualValues(repl.Replications[i].Events, repl.Replications[i].EventsDone)
 		r.NotNil(repl.Replications[i].LastProcessedAt)
 		r.NotNil(repl.Replications[i].LastEmittedAt)
+
+		r.False(repl.Replications[i].InitDoneAt.AsTime().IsZero())
+		r.True(
+			repl.Replications[i].InitDoneAt.AsTime().After(repl.Replications[i].CreatedAt.AsTime()) ||
+				repl.Replications[i].InitDoneAt.AsTime().Equal(repl.Replications[i].CreatedAt.AsTime()),
+		)
+
 		//		r.True(repl.Replications[i].LastProcessedAt.AsTime().Equal(repl.Replications[i].LastEmittedAt.AsTime()))
 		r.False(repl.Replications[i].LastEmittedAt.AsTime().IsZero())
 		r.False(repl.Replications[i].LastProcessedAt.AsTime().IsZero())
@@ -571,6 +578,10 @@ func TestApi_Migrate_test(t *testing.T) {
 		r.EqualValues(repl.Replications[i].Events, repl.Replications[i].EventsDone)
 		r.NotNil(repl.Replications[i].LastProcessedAt)
 		r.NotNil(repl.Replications[i].LastEmittedAt)
+		if repl.Replications[i].IsInitDone {
+			r.False(repl.Replications[i].InitDoneAt.AsTime().IsZero())
+			r.True(repl.Replications[i].InitDoneAt.AsTime().After(repl.Replications[i].CreatedAt.AsTime()) || repl.Replications[i].InitDoneAt.AsTime().Equal(repl.Replications[i].CreatedAt.AsTime()))
+		}
 		//		r.True(repl.Replications[i].LastProcessedAt.AsTime().Equal(repl.Replications[i].LastEmittedAt.AsTime()))
 		r.False(repl.Replications[i].LastEmittedAt.AsTime().IsZero())
 		r.False(repl.Replications[i].LastProcessedAt.AsTime().IsZero())
