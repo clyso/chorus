5fb127b4a34ce9c3cff4890e6fe797a4c2effaa5
Evgenii Koniakhin <evgenii.koniakhin@clyso.com>
2025-03-04 18:27:12 +0100

    chore: fix api prefix

--- ui/src/utils/helpers/ApiHelper.ts
+++ ui/src/utils/helpers/ApiHelper.ts
@@ -15,11 +15,11 @@
  */
 
 import { ApiVersion } from '../types/api';
-import { API_BASE_URL } from '@/utils/constants/env';
+import { API_BASE_URL, API_PREFIX } from '@/utils/constants/env';
 
 export abstract class ApiHelper {
   static getChorusAPIUrl(resourcePath: string): string {
-    return `${API_BASE_URL}${resourcePath}`;
+    return `${API_BASE_URL}${API_PREFIX}${resourcePath}`;
   }
 
   static getPrometheusAPIUrl(
@@ -27,9 +27,5 @@ export abstract class ApiHelper {
     version = ApiVersion.V1Beta1,
   ): string {
     return `${API_BASE_URL}/prometheus/api/${version}${resourcePath}`;
-  }
-
-  static getApiAuthUrl(resourcePath: string): string {
-    return `${API_BASE_URL}${resourcePath}`;
   }
 }
