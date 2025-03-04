8e836cb2bb2cffad9604f7f3c00ccdc4f9792384
Evgenii Koniakhin <evgenii.koniakhin@clyso.com>
2025-03-04 15:50:04 +0100

    chore: add license

--- ui/src/components/chorus/replications/UserReplicationDeletionDialog/UserReplicationDeletionDialog.vue
+++ ui/src/components/chorus/replications/UserReplicationDeletionDialog/UserReplicationDeletionDialog.vue
@@ -1,3 +1,19 @@
+<!--
+  - Copyright © 2025 Clyso GmbH
+  -
+  -  Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
+  -  you may not use this file except in compliance with the License.
+  -  You may obtain a copy of the License at
+  -
+  -  https://www.gnu.org/licenses/agpl-3.0.html
+  -
+  -  Unless required by applicable law or agreed to in writing, software
+  -  distributed under the License is distributed on an "AS IS" BASIS,
+  -  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  -  See the License for the specific language governing permissions and
+  -  limitations under the License.
+  -->
+
 <script setup lang="ts">
   import { CDialog, CCheckbox } from '@clyso/clyso-ui-kit';
   import { storeToRefs } from 'pinia';
