ffdd0484fe5f7a82657d75dd3aeb9cf94b106d5d
Evgenii Koniakhin <evgenii.koniakhin@clyso.com>
2025-03-04 18:27:12 +0100

    chore: fix api prefix

--- ui/src/utils/constants/env.ts
+++ ui/src/utils/constants/env.ts
@@ -15,6 +15,7 @@
  */
 
 export const IS_DEV_ENV = import.meta.env.DEV;
-export const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL;
 export const HAS_PROMETHEUS = import.meta.env.VITE_HAS_PROMETHEUS === 'TRUE';
 export const HAS_LANG_SELECT = import.meta.env.VITE_HAS_LANG_SELECT === 'TRUE';
+export const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL ?? '';
+export const API_PREFIX: string = import.meta.env.VITE_API_PREFIX ?? '';
