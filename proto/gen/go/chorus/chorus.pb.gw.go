0bef929dc7b6d50f3d5d134944e3e2e2c75885a5
shutstart <chirag.cool200@gmail.com>
2025-03-12 10:06:43 +0100

    rename method and endpoint

--- proto/gen/go/chorus/chorus.pb.gw.go
+++ proto/gen/go/chorus/chorus.pb.gw.go
@@ -360,7 +360,7 @@ func local_request_Chorus_AddBucketReplication_0(ctx context.Context, marshaler 
 	return msg, metadata, err
 }
 
-func request_Chorus_FetchReplicationStatus_0(ctx context.Context, marshaler runtime.Marshaler, client ChorusClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func request_Chorus_GetReplicationStatus_0(ctx context.Context, marshaler runtime.Marshaler, client ChorusClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
 		protoReq ReplicationRequest
 		metadata runtime.ServerMetadata
@@ -368,11 +368,11 @@ func request_Chorus_FetchReplicationStatus_0(ctx context.Context, marshaler runt
 	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	msg, err := client.FetchReplicationStatus(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.GetReplicationStatus(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 }
 
-func local_request_Chorus_FetchReplicationStatus_0(ctx context.Context, marshaler runtime.Marshaler, server ChorusServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func local_request_Chorus_GetReplicationStatus_0(ctx context.Context, marshaler runtime.Marshaler, server ChorusServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
 		protoReq ReplicationRequest
 		metadata runtime.ServerMetadata
@@ -380,7 +380,7 @@ func local_request_Chorus_FetchReplicationStatus_0(ctx context.Context, marshale
 	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	msg, err := server.FetchReplicationStatus(ctx, &protoReq)
+	msg, err := server.GetReplicationStatus(ctx, &protoReq)
 	return msg, metadata, err
 }
 
@@ -690,25 +690,25 @@ func RegisterChorusHandlerServer(ctx context.Context, mux *runtime.ServeMux, ser
 		}
 		forward_Chorus_AddBucketReplication_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
-	mux.Handle(http.MethodPost, pattern_Chorus_FetchReplicationStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle(http.MethodPost, pattern_Chorus_GetReplicationStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/chorus.Chorus/FetchReplicationStatus", runtime.WithHTTPPathPattern("/replication/status/fetch"))
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/chorus.Chorus/GetReplicationStatus", runtime.WithHTTPPathPattern("/replication/get"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Chorus_FetchReplicationStatus_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Chorus_GetReplicationStatus_0(annotatedContext, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		forward_Chorus_FetchReplicationStatus_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Chorus_GetReplicationStatus_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	return nil
@@ -1005,22 +1005,22 @@ func RegisterChorusHandlerClient(ctx context.Context, mux *runtime.ServeMux, cli
 		}
 		forward_Chorus_AddBucketReplication_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
-	mux.Handle(http.MethodPost, pattern_Chorus_FetchReplicationStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle(http.MethodPost, pattern_Chorus_GetReplicationStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/chorus.Chorus/FetchReplicationStatus", runtime.WithHTTPPathPattern("/replication/status/fetch"))
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/chorus.Chorus/GetReplicationStatus", runtime.WithHTTPPathPattern("/replication/get"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Chorus_FetchReplicationStatus_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Chorus_GetReplicationStatus_0(annotatedContext, inboundMarshaler, client, req, pathParams)
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		forward_Chorus_FetchReplicationStatus_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Chorus_GetReplicationStatus_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 	return nil
 }
@@ -1041,7 +1041,7 @@ var (
 	pattern_Chorus_CompareBucket_0             = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"replication", "compare-bucket"}, ""))
 	pattern_Chorus_GetAgents_0                 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"agents"}, ""))
 	pattern_Chorus_AddBucketReplication_0      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v2", "replication", "add"}, ""))
-	pattern_Chorus_FetchReplicationStatus_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"replication", "status", "fetch"}, ""))
+	pattern_Chorus_GetReplicationStatus_0      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"replication", "get"}, ""))
 )
 
 var (
@@ -1060,5 +1060,5 @@ var (
 	forward_Chorus_CompareBucket_0             = runtime.ForwardResponseMessage
 	forward_Chorus_GetAgents_0                 = runtime.ForwardResponseMessage
 	forward_Chorus_AddBucketReplication_0      = runtime.ForwardResponseMessage
-	forward_Chorus_FetchReplicationStatus_0    = runtime.ForwardResponseMessage
+	forward_Chorus_GetReplicationStatus_0      = runtime.ForwardResponseMessage
 )
