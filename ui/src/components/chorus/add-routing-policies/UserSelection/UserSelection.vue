33dd994a7acfac2b29c0797066d809f90cb4a5d4
Tatjana Dehler <tatjana.dehler@clyso.com>
2026-03-09 10:44:39 +0100

    feat(ui): add user selection
    Add the user selection to the routing policy creation form.
    
    Signed-off-by: Tatjana Dehler <tatjana.dehler@clyso.com>
    On-behalf-of: SAP t.dehler@sap.com

---
+++ ui/src/components/chorus/add-routing-policies/UserSelection/UserSelection.vue
@@ -0,0 +1,60 @@
+<!--
+ - Copyright © 2026 Clyso GmbH
+ -
+ -  Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
+ -  you may not use this file except in compliance with the License.
+ -  You may obtain a copy of the License at
+ -
+ -  https://www.gnu.org/licenses/agpl-3.0.html
+ -
+ -  Unless required by applicable law or agreed to in writing, software
+ -  distributed under the License is distributed on an "AS IS" BASIS,
+ -  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ -  See the License for the specific language governing permissions and
+ -  limitations under the License.
+ -->
+
+<script setup lang="ts">
+  import { storeToRefs } from 'pinia';
+  import { useI18n } from 'vue-i18n';
+  import i18nAddRoutingPolicy from '../i18nAddRoutingPolicy';
+  import ChorusUserCardList from '../../common/ChorusUserCardList/ChorusUserCardList.vue';
+  import { useChorusAddRoutingPolicyStore } from '@/stores/chorusAddRoutingPolicyStore';
+
+  const { selectedUser, users } = storeToRefs(useChorusAddRoutingPolicyStore());
+
+  const { t } = useI18n({
+    messages: i18nAddRoutingPolicy,
+  });
+</script>
+
+<template>
+  <div class="user-selection">
+    <div class="user-selection__header">
+      <h5 class="user-selection__title">{{ t('userSelectionTitle') }}</h5>
+      <p class="user-selection__description">
+        {{ t('userSelectionDescription') }}
+      </p>
+    </div>
+
+    <div class="user-selection__content">
+      <div class="user-selection__selector">
+        <ChorusUserCardList
+          v-model="selectedUser"
+          :users="users"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '@/styles/utils' as utils;
+
+  .user-selection {
+    &__title,
+    &__description {
+      margin-bottom: utils.unit(1);
+    }
+  }
+</style>
