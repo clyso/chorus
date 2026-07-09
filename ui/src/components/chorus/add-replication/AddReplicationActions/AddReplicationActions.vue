07b8b28962393d805d561ccd81c639c85ca5255e
Tatjana Dehler <tatjana.dehler@clyso.com>
2026-07-09 11:59:35 +0200

    refactor(ui): add ChorusWizard component
    Add a new component `ChorusWizard` to be re-used as part of the add
    replications and add diff report functionality.
    
    Signed-off-by: Tatjana Dehler <tatjana.dehler@clyso.com>
    On-behalf-of: SAP <t.dehler@sap.com>

--- ui/src/components/chorus/add-replication/AddReplicationActions/AddReplicationActions.vue
+++ ui/src/components/chorus/add-replication/AddReplicationActions/AddReplicationActions.vue
@@ -15,14 +15,13 @@
   -->
 
 <script setup lang="ts">
-  import { CButton, CIcon } from '@clyso/clyso-ui-kit';
   import { useI18n } from 'vue-i18n';
   import { computed } from 'vue';
   import { storeToRefs } from 'pinia';
   import i18nAddReplication from '@/components/chorus/add-replication/i18nAddReplication';
-  import { IconName } from '@/utils/types/icon';
   import { useChorusAddReplicationStore } from '@/stores/chorusAddReplicationStore';
   import { AddReplicationStepName } from '@/utils/types/chorus';
+  import ChorusWizard from '@/components/chorus/common/ChorusWizard/ChorusWizard.vue';
 
   const { t } = useI18n({
     messages: i18nAddReplication,
@@ -40,13 +39,8 @@
     selectedUser,
   } = storeToRefs(useChorusAddReplicationStore());
 
-  const isLastStep = computed<boolean>(
-    () => currentStep.value === stepsCount.value,
-  );
-  const isFirstStep = computed<boolean>(() => currentStep.value === 1);
-
   const isNextDisabled = computed<boolean>(() => {
-    if (isLoading.value || isLastStep.value) {
+    if (isLoading.value || currentStep.value === stepsCount.value) {
       return true;
     }
 
@@ -65,22 +59,6 @@
     return false;
   });
 
-  function goNext() {
-    if (isNextDisabled.value) {
-      return;
-    }
-
-    currentStep.value += 1;
-  }
-
-  function goBack() {
-    if (isFirstStep.value) {
-      return;
-    }
-
-    currentStep.value -= 1;
-  }
-
   function handleSubmit() {
     validator.value.$touch();
 
@@ -93,59 +71,13 @@
 </script>
 
 <template>
-  <div class="add-replication-actions">
-    <CButton
-      secondary
-      :disabled="isFirstStep || isSubmitting"
-      size="large"
-      class="add-replication-actions__back"
-      :loading="false"
-      @click="goBack"
-    >
-      <template #icon>
-        <CIcon
-          :is-inline="true"
-          :name="IconName.BASE_ARROW_BACK"
-        />
-      </template>
-    </CButton>
-
-    <CButton
-      v-if="!isLastStep"
-      type="primary"
-      ghost
-      size="large"
-      :disabled="isNextDisabled"
-      class="add-replication-actions__next"
-      @click="goNext"
-    >
-      <template #icon>
-        <CIcon
-          :is-inline="true"
-          :name="IconName.BASE_ARROW_FORWARD"
-        />
-      </template>
-      {{ t('nextAction') }}
-    </CButton>
-    <CButton
-      v-else
-      type="primary"
-      :disabled="isSubmitting"
-      :loading="isSubmitting"
-      size="large"
-      class="add-replication-actions__submit"
-      @click="handleSubmit"
-    >
-      {{ t('addReplicationAction') }}
-    </CButton>
-  </div>
+  <ChorusWizard
+    v-model:current-step="currentStep"
+    :steps-count="stepsCount"
+    :is-submitting="isSubmitting"
+    :is-next-disabled="isNextDisabled"
+    :submit-label="t('addReplicationAction')"
+    :next-label="t('nextAction')"
+    @submit="handleSubmit"
+  />
 </template>
-
-<style lang="scss" scoped>
-  @use '@/styles/utils' as utils;
-
-  .add-replication-actions {
-    display: flex;
-    gap: utils.unit(3);
-  }
-</style>
